0129f706dcb5baac80e93a16f4632c6c8d735907
alexiscyber14 <142sledgehammer14@gmail.com>
2023-08-12 08:40:26 +0100

    add c

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-if( "serviceWorker" in navigator){
-  navigator.serviceWorker.register("sw.js").then(registration=>{
-    console.log("sw registered");
-    console.log(registration)
-  }).catch(error => {
-    console.log("sw registration failed!");
-    console.log(error);
-  })
-}
 
 window.scrollTo(0,1)
 
